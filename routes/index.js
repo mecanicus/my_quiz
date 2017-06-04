7b5b2507e3f3ca7ca3feb73393194a90861a5350
mecanicus <fernandomonjer@gmail.com>
2017-06-04 17:11:03 +0200

    practica52

--- routes/index.js
+++ routes/index.js
@@ -13,22 +13,30 @@ router.get('/author', function(req, res, next) {
     res.render('author');
 });
 
+// Pagina de ayuda
+router.get('/help', function(req, res, next) {
+    res.render('help');
+});
+
 
 // Autoload de rutas que usen :quizId
 router.param('quizId', quizController.load);
 
 
 // Definición de rutas de /quizzes
-router.get('/quizzes',                     quizController.index);
-router.get('/quizzes/:quizId(\\d+)',       quizController.show);
-router.get('/quizzes/new',                 quizController.new);
-router.post('/quizzes',                    quizController.create);
-router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
-router.put('/quizzes/:quizId(\\d+)',       quizController.update);
-router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
+router.get('/quizzes',                     		quizController.index);
+router.get('/quizzes/:quizId(\\d+)',       		quizController.show);
+router.get('/quizzes/new',                 		quizController.new);
+router.post('/quizzes',                    		quizController.create);
+router.get('/quizzes/:quizId(\\d+)/edit',  		quizController.edit);
+router.put('/quizzes/:quizId(\\d+)',       		quizController.update);
+router.delete('/quizzes/:quizId(\\d+)',    		quizController.destroy);
 
-router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
-router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
+router.get('/quizzes/:quizId(\\d+)/play',  		quizController.play);
+router.get('/quizzes/:quizId(\\d+)/check', 		quizController.check);
+
+router.get('/quizzes/randomplay',	   		quizController.randomplay);
+router.get('/quizzes/randomcheck/:quizId(\\d+)',	quizController.randomcheck);
 
 
 module.exports = router;
